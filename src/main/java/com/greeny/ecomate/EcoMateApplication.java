8e6b67eb727bd2f2bb6adb302ec85c27cf09c149
parkjiyun <pjyun05200@gmail.com>
2023-06-27 00:13:28 +0900

    Establish: 프로젝트 초기 설정

---
+++ src/main/java/com/greeny/ecomate/EcoMateApplication.java
@@ -0,0 +1,13 @@
+package com.greeny.ecomate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcoMateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcoMateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/greeny/ecomate/EcoMateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.greeny.ecomate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcoMateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
