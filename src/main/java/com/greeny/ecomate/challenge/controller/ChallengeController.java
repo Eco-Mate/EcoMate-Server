6a49894cd542159ae5af0f52494729d8a92dc464
parkjiyun <pjyun05200@gmail.com>
2023-07-18 19:34:48 +0900

    Refactor: challengeController, myChallengeController에 ApiUtil 적용

--- src/main/java/com/greeny/ecomate/challenge/controller/ChallengeController.java
+++ src/main/java/com/greeny/ecomate/challenge/controller/ChallengeController.java
@@ -4,6 +4,11 @@ import com.greeny.ecomate.challenge.dto.ChallengeDto;
 import com.greeny.ecomate.challenge.dto.CreateChallengeRequestDto;
 import com.greeny.ecomate.challenge.entity.Challenge;
 import com.greeny.ecomate.challenge.service.ChallengeService;
+import com.greeny.ecomate.utils.api.ApiUtil;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
@@ -21,43 +26,43 @@ public class ChallengeController {
     private final ChallengeService challengeService;
 
     @PostMapping("/form")
-    public Long createNewChallenge(@RequestBody CreateChallengeRequestDto dto) {
-        return challengeService.createChallenge(dto);
+    public ApiUtil.ApiSuccessResult<Long> createNewChallenge(@Valid @RequestBody CreateChallengeRequestDto dto) {
+        return ApiUtil.success("챌린지 생성 성공", challengeService.createChallenge(dto));
     }
 
     @GetMapping("/{challengeId}")
-    public Challenge getChallengeById(@PathVariable Long challengeId,
+    public ApiUtil.ApiSuccessResult<Challenge> getChallengeById(@PathVariable Long challengeId,
                                       HttpServletRequest req) {
-        return challengeService.getChallengeById(challengeId);
+        return ApiUtil.success("챌린지 조회 성공", challengeService.getChallengeById(challengeId));
     }
 
     @GetMapping
-    public List<ChallengeDto> getAllChallenge() {
-        return challengeService.findAllChallenge();
+    public ApiUtil.ApiSuccessResult<List<ChallengeDto>> getAllChallenge() {
+        return ApiUtil.success("챌린지 전체 조회 성공", challengeService.findAllChallenge());
     }
 
     @PutMapping("/activeYn/{challengeId}")
-    public Long updateChallengeActiveYn(@PathVariable Long challengeId,
+    public ApiUtil.ApiSuccessResult<Long> updateChallengeActiveYn(@PathVariable Long challengeId,
                                         @RequestBody boolean activeYn,
                                         HttpServletRequest req) {
         challengeService.updateChallengeActiveYn(challengeId, activeYn);
-        return challengeId;
+        return ApiUtil.success("챌린지 활성화 여부 수정 성공", challengeId);
     }
 
     @PutMapping("/{challengeId}")
-    public Long updateChallenge(@PathVariable Long challengeId,
+    public ApiUtil.ApiSuccessResult<Long> updateChallenge(@PathVariable Long challengeId,
                                 @Valid @RequestBody ChallengeDto challengeDto,
                                 HttpServletRequest req) {
 
         challengeService.updateChallenge(challengeId, challengeDto);
-        return challengeId;
+        return ApiUtil.success("챌린지 수정 성공", challengeId);
     }
 
     @DeleteMapping("/{challengeId}")
-    public String deleteChallenge(@PathVariable Long challengeId,
+    public ApiUtil.ApiSuccessResult<String> deleteChallenge(@PathVariable Long challengeId,
                                   HttpServletRequest req) {
         challengeService.deleteChallenge(challengeId);
-        return "삭제되었습니다.";
+        return ApiUtil.success("챌린지 삭제 성공", "삭제되었습니다.");
     }
 
 }
--- src/main/java/com/greeny/ecomate/challenge/controller/MyChallengeController.java
+++ src/main/java/com/greeny/ecomate/challenge/controller/MyChallengeController.java
@@ -3,10 +3,12 @@ package com.greeny.ecomate.challenge.controller;
 import com.greeny.ecomate.challenge.dto.CreateMyChallengeRequestDto;
 import com.greeny.ecomate.challenge.dto.MyChallengeDto;
 import com.greeny.ecomate.challenge.service.MyChallengeService;
+import com.greeny.ecomate.utils.api.ApiUtil;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
@@ -18,13 +20,13 @@ public class MyChallengeController {
     private final MyChallengeService myChallengeService;
 
     @PostMapping("/form")
-    public Long createMyChallenge(@RequestBody CreateMyChallengeRequestDto dto) {
-        return myChallengeService.createMyChallenge(dto);
+    public ApiUtil.ApiSuccessResult<Long> createMyChallenge(@Valid @RequestBody CreateMyChallengeRequestDto dto) {
+        return ApiUtil.success("챌린지 도전 시작 성공", myChallengeService.createMyChallenge(dto));
     }
 
     @GetMapping("/{userId}")
-    public List<MyChallengeDto> getMyChallengeByUserId(@PathVariable Long userId) {
-        return myChallengeService.getMyChallengeByUserId(userId);
+    public ApiUtil.ApiSuccessResult<List<MyChallengeDto>> getMyChallengeByUserId(@PathVariable Long userId) {
+        return ApiUtil.success("사용자별 도전 챌린지 조회 성공", myChallengeService.getMyChallengeByUserId(userId));
     }
 
 }
