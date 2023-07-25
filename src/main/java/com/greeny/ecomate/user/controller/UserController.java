4d4938ce6aada51a42cb790b76e44f83044f63f0
parkjiyun <pjyun05200@gmail.com>
2023-07-26 07:34:16 +0900

    Chore: UserController에 태그 추가

--- src/main/java/com/greeny/ecomate/user/controller/UserController.java
+++ src/main/java/com/greeny/ecomate/user/controller/UserController.java
@@ -2,15 +2,14 @@ package com.greeny.ecomate.user.controller;
 
 import com.greeny.ecomate.user.dto.CreateUserRequestDto;
 import com.greeny.ecomate.user.service.UserService;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/v1/user")
 @RequiredArgsConstructor
+@Tag(name = "User")
 public class UserController {
 
     private final UserService userService;
