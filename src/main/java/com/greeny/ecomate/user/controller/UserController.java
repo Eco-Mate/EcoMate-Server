a83523f285a0543e408d58ba5584459e6d80da38
parkjiyun <pjyun05200@gmail.com>
2023-07-26 07:37:56 +0900

    Refacotr: UserController에 ApiUtil 적용

--- src/main/java/com/greeny/ecomate/user/controller/UserController.java
+++ src/main/java/com/greeny/ecomate/user/controller/UserController.java
@@ -2,6 +2,8 @@ package com.greeny.ecomate.user.controller;
 
 import com.greeny.ecomate.user.dto.CreateUserRequestDto;
 import com.greeny.ecomate.user.service.UserService;
+import com.greeny.ecomate.utils.api.ApiUtil;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
@@ -14,9 +16,10 @@ public class UserController {
 
     private final UserService userService;
 
+    @ApiResponse(description = "사용자 생성")
     @PostMapping
-    public Long createUser(@RequestBody CreateUserRequestDto createDto) {
-        return userService.createUser(createDto);
+    public ApiUtil.ApiSuccessResult<Long> createUser(@RequestBody CreateUserRequestDto createDto) {
+        return ApiUtil.success("사용자 생성 성공", userService.createUser(createDto));
     }
 
 }
