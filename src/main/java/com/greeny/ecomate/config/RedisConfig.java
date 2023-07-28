2d908be7ed646f588144d4a08bce4f9fa6364e0b
parkjiyun <pjyun05200@gmail.com>
2023-07-28 22:36:59 +0900

    Feat: RedisConfig, RedisUtil 수정

--- src/main/java/com/greeny/ecomate/config/RedisConfig.java
+++ src/main/java/com/greeny/ecomate/config/RedisConfig.java
@@ -1,5 +1,6 @@
 package com.greeny.ecomate.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,9 +14,14 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 @EnableCaching
 public class RedisConfig {
 
+    @Value("${spring.redis.host}")
+    private String host;
+    @Value("${spring.redis.port}")
+    private int port;
+
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory();
+        return new LettuceConnectionFactory(host, port);
     }
 
     @Bean
@@ -25,7 +31,6 @@ public class RedisConfig {
         RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
         redisTemplate.setConnectionFactory(redisConnectionFactory);
         redisTemplate.setKeySerializer(new StringRedisSerializer());
-
         redisTemplate.setValueSerializer(genericJackson2JsonRedisSerializer);
 
         return redisTemplate;
