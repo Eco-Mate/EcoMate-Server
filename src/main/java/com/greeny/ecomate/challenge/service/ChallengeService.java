1afc0fbd2d9a8054a1d9b52723fd15b17340209f
parkjiyun <pjyun05200@gmail.com>
2023-07-30 18:59:50 +0900

    Refactor: Challenge 관련 DTO를 recode에서 class로 변경

--- src/main/java/com/greeny/ecomate/challenge/service/ChallengeService.java
+++ src/main/java/com/greeny/ecomate/challenge/service/ChallengeService.java
@@ -3,6 +3,7 @@ package com.greeny.ecomate.challenge.service;
 
 import com.greeny.ecomate.challenge.dto.ChallengeDto;
 import com.greeny.ecomate.challenge.dto.CreateChallengeRequestDto;
+import com.greeny.ecomate.challenge.dto.UpdateChallengeRequestDto;
 import com.greeny.ecomate.challenge.entity.AchieveType;
 import com.greeny.ecomate.challenge.entity.Challenge;
 import com.greeny.ecomate.challenge.repository.ChallengeRepository;
@@ -25,22 +26,33 @@ public class ChallengeService {
 
     @Transactional
     public Long createChallenge(CreateChallengeRequestDto dto, Member member) {
-        if(member.getRole() != Role.ROLE_ADMIN) {
+        if(member.getRole() != Role.ROLE_ADMIN)
             throw new IllegalStateException("챌린지 생성 권한이 없습니다.");
-        }
-        Challenge challenge = dto.toEntity();
+
+        Challenge challenge = Challenge.builder()
+                .activeYn(dto.getActiveYn())
+                .challengeTitle(dto.getChallengeTitle())
+                .description(dto.getDescription())
+                .goalCnt(dto.getGoalCnt())
+                .treePoint(dto.getTreePoint())
+                .build();
+
         return challengeRepository.save(challenge).getChallengeId();
     }
 
     public Challenge getChallengeById(Long challengeId) {
         Challenge findChallenge = challengeRepository.findById(challengeId)
-                .orElseThrow(() -> new IllegalArgumentException("챌린지를 찾을 수 없습니다."));
+                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 챌린지입니다."));
         return findChallenge;
+    }
+
+    private ChallengeDto createChallengeDto(Challenge challenge) {
+        return new ChallengeDto(challenge);
     }
 
     public List<ChallengeDto> findAllChallenge() {
         List<Challenge> challengeList = challengeRepository.findAll();
-        return challengeList.stream().map(ChallengeDto::from).toList();
+        return challengeList.stream().map(this::createChallengeDto).toList();
     }
 
     public Long getProceedingChallengeCnt(Long challengeId) {
@@ -49,39 +61,38 @@ public class ChallengeService {
 
     @Transactional
     public void updateChallengeActiveYn(Long challengeId, boolean activeYn, Member member) {
-        if(member.getRole() != Role.ROLE_ADMIN) {
+        if(member.getRole() != Role.ROLE_ADMIN)
             throw new IllegalStateException("챌린지 활성화 수정 권한이 없습니다.");
-        }
 
         Challenge challenge = challengeRepository.findById(challengeId)
-                .orElseThrow(() -> new IllegalArgumentException("챌린지가 존재하지 않습니다."));
+                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 챌린지입니다."));
 
         challenge.updateActiveYn(activeYn);
     }
 
     @Transactional
-    public void updateChallenge(Long challengeId, ChallengeDto challengeDto, Member member) {
-        if(member.getRole() != Role.ROLE_ADMIN) {
+    public Long updateChallenge(Long challengeId, UpdateChallengeRequestDto dto, Member member) {
+        if(member.getRole() != Role.ROLE_ADMIN)
             throw new IllegalStateException("챌린지 내용 수정 권한이 없습니다.");
-        }
 
         Challenge challenge = challengeRepository.findById(challengeId)
-                .orElseThrow(() -> new IllegalArgumentException("챌린지가 존재하지 않습니다."));
+                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 챌린지입니다."));
 
-        challenge.updateTitle(challengeDto.challengeTitle());
-        challenge.updateDescription(challengeDto.description());
-        challenge.updateGoalCnt(challengeDto.goalCnt());
-        challenge.updateTreePoint(challengeDto.treePoint());
+        challenge.updateTitle(dto.getChallengeTitle());
+        challenge.updateDescription(dto.getDescription());
+        challenge.updateGoalCnt(dto.getGoalCnt());
+        challenge.updateTreePoint(dto.getTreePoint());
+        return challenge.getChallengeId();
     }
 
     @Transactional
     public void deleteChallenge(Long challengeId, Member member) {
-        if(member.getRole() != Role.ROLE_ADMIN) {
+        if(member.getRole() != Role.ROLE_ADMIN)
             throw new IllegalStateException("챌린지 삭제 권한이 없습니다.");
-        }
 
         Challenge challenge = challengeRepository.findById(challengeId)
                 .orElseThrow(() -> new IllegalArgumentException("챌린지가 존재하지 않습니다."));
+
         challengeRepository.delete(challenge);
     }
 
