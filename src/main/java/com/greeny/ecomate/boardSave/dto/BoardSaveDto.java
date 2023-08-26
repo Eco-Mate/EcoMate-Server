36cc41fa665fd67241cf55bc2e2e298e833a6a4a
lee-ji-an <leeja0407@naver.com>
2023-08-26 23:07:36 +0900

    Feat: 게시물 저장 기능의 응답 수정

---
+++ src/main/java/com/greeny/ecomate/boardSave/dto/BoardSaveDto.java
@@ -0,0 +1,10 @@
+package com.greeny.ecomate.boardSave.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class BoardSaveDto {
+    private Boolean saved;
+}
