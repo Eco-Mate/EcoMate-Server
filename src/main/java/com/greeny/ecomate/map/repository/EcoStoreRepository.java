40aad09ace9093c958ee5f8704ccbc6d364f534f
parkjiyun <pjyun05200@gmail.com>
2023-09-02 00:11:39 +0900

    Feat: EcoStore 레포지토리 추가

---
+++ src/main/java/com/greeny/ecomate/map/repository/EcoStoreRepository.java
@@ -0,0 +1,8 @@
+package com.greeny.ecomate.map.repository;
+
+import com.greeny.ecomate.map.entity.EcoStore;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EcoStoreRepository extends JpaRepository<EcoStore, Long> {
+
+}
