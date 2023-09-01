87ac020e63ef61c31e4da9a70038d9ab2e4c0935
lee-ji-an <leeja0407@naver.com>
2023-09-01 21:42:29 +0900

    Fix: firebase 서비스키 파일 접근 방법 수정

--- src/main/java/com/greeny/ecomate/fcm/FirebaseMessageInitializer.java
+++ src/main/java/com/greeny/ecomate/fcm/FirebaseMessageInitializer.java
@@ -4,10 +4,12 @@ import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
 import java.io.FileInputStream;
+import java.io.InputStream;
 
 @Service
 public class FirebaseMessageInitializer {
@@ -18,8 +20,8 @@ public class FirebaseMessageInitializer {
     @PostConstruct
     public void initialize() {
         try {
-            FileInputStream serviceAccount =
-                    new FileInputStream(FIREBASE_CONFIG_PATH);
+            InputStream serviceAccount =
+                    new ClassPathResource(FIREBASE_CONFIG_PATH).getInputStream();
 
             FirebaseOptions options = FirebaseOptions.builder()
                     .setCredentials(GoogleCredentials.fromStream(serviceAccount))
