450c3339f740d9c1ae754f6c61b0876179dcfe34
parkjiyun <pjyun05200@gmail.com>
2023-09-02 13:40:27 +0900

    StoreLike 레포지토리 추가

---
+++ src/main/java/com/greeny/ecomate/map/repository/StoreLikeRepository.java
@@ -0,0 +1,8 @@
+package com.greeny.ecomate.map.repository;
+
+import com.greeny.ecomate.map.entity.StoreLike;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StoreLikeRepository extends JpaRepository<StoreLike, Long> {
+
+}
