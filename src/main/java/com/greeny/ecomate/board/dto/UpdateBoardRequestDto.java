2f68d30f66682d87576dcc1df58ace3e1e589b50
lee-ji-an <leeja0407@naver.com>
2023-10-25 11:59:56 +0900

    Chore: 테스트를 위해 게시물 수정 요청 dto에 생성자 추가

--- src/main/java/com/greeny/ecomate/board/dto/UpdateBoardRequestDto.java
+++ src/main/java/com/greeny/ecomate/board/dto/UpdateBoardRequestDto.java
@@ -1,10 +1,12 @@
 package com.greeny.ecomate.board.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 import javax.validation.constraints.NotNull;
 
 @Data
+@AllArgsConstructor
 public class UpdateBoardRequestDto {
     // image, challenge 는 수정 불가
 
