fffadde9b1a1990bf6f5b4f84aa97d6e62f26367
lee-ji-an <leeja0407@naver.com>
2023-10-27 14:57:58 +0900

    Chore: 불필요한 import 제거

--- src/test/java/com/greeny/ecomate/board/service/BoardServiceTest.java
+++ src/test/java/com/greeny/ecomate/board/service/BoardServiceTest.java
@@ -22,13 +22,11 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.util.ReflectionTestUtils;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.List;
 import java.util.Optional;
